794d8c29be018c00241834cf8a88f6e3938735c4
Fachri26 <fachriwiryansyah26@gmail.com>
2026-04-29 22:55:11 +0700

    feat: impelement jwt token dan inisialisasi routes

---
+++ services/auth-service/src/routes/authRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  register,
+  login,
+  refreshToken,
+  logout,
+  profile
+} = require('../controllers/authController');
+
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.post('/register', register);
+router.post('/login', login);
+router.post('/refresh', refreshToken);
+router.post('/logout', logout);
+router.get('/profile', authMiddleware, profile);
+
+module.exports = router;
