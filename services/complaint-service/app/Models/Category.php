8eb39038a364e03105f93cc3974fb8143d7d1360
Fachri26 <fachriwiryansyah26@gmail.com>
2026-04-30 22:05:02 +0700

    feat: implement MVC

--- services/complaint-service/app/Models/Category.php
+++ services/complaint-service/app/Models/Category.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Complaint;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    public function complaints() {
+        return $this->hasMany(Complaint::class);
+    }
 }
--- services/complaint-service/app/Models/Rating.php
+++ services/complaint-service/app/Models/Rating.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Complaint;
 
 class Rating extends Model
 {
-    //
+    protected $fillable = [
+        'complaint_id',
+        'rating',
+        'feedback'
+    ];
+
+    public function complaint() {
+        return $this->belongsTo(Complaint::class);
+    }
 }
