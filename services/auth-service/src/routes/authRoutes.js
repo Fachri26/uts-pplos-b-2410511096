22d9beed52f3f68b0110bb22bc99696600cffc47
Fachri26 <fachriwiryansyah26@gmail.com>
2026-04-30 20:00:10 +0700

    feat: OAuth google

--- services/auth-service/src/routes/authRoutes.js
+++ services/auth-service/src/routes/authRoutes.js
@@ -12,11 +12,38 @@ const {
 
 const authMiddleware = require('../middleware/authMiddleware');
 
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
 router.post('/register', register);
 router.post('/login', login);
 router.post('/refresh', refreshToken);
 router.post('/logout', logout);
 router.delete('/delete', authMiddleware, deleteUser);
 router.get('/profile', authMiddleware, profile);
+
+router.get('/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get('/google/callback',
+  passport.authenticate('google', { session: false, failureRedirect: '/' }),
+  (req, res) => {
+
+    const payload = {
+      id: req.user.id,
+      email: req.user.email
+    };
+
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRES
+    });
+
+    res.json({
+      message: 'Login Google berhasil',
+      accessToken
+    });
+  }
+);
 
 module.exports = router;
